fac35a4830367c94eecbad381a2a4ebcbc14d840
takumadx <hirayama.takuma@ohmydigifab.com>
2018-01-22 14:10:53 +0900

    asv

--- asv/split.sh
+++ asv/split.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-x_max=$2
-y_max=$3
+y_max=$2
+x_max=$3
 for ((y=0; y < $y_max; y++)); do
 	for ((x=0; x < $x_max; x++)); do
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o ${1%.*}_x${x}_y${y}.stl -D x=$x -D y=$y -D target=\"$1\" split.scad
+		/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o ${1%.*}_y${y}_x${x}.stl -D y=$y -D x=$x -D target=\"$1\" split.scad
 	done
 done
